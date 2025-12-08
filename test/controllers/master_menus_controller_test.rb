f7fb7cbdf6854d7b9dadfd5b4d2e51276321afb6
yuka <214949744+yuka-p@users.noreply.github.com>
2025-12-08 11:14:48 +0900

    test: fix index test error

--- test/controllers/master_menus_controller_test.rb
+++ test/controllers/master_menus_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class MasterMenusControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)  # fixtures で user を用意
+    sign_in @user
+  end
+
   test "should get index" do
     get master_menus_index_url
     assert_response :success

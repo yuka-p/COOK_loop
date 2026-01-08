b9cc2213fa6ae3f92340c2ba4b7c1dbb60d97ac9
yuka <214949744+yuka-p@users.noreply.github.com>
2026-01-08 13:56:17 +0900

    fix :ルーティングのテストコード修正

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,12 +2,12 @@ require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get terms" do
-    get pages_terms_url
+    get terms_url
     assert_response :success
   end
 
   test "should get privacy" do
-    get pages_privacy_url
+    get privacy_url
     assert_response :success
   end
 end

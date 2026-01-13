2004011eee37f14eba1ed93b788d6d0581013f3e
yuka <214949744+yuka-p@users.noreply.github.com>
2026-01-13 13:17:23 +0900

    style: マイメニュー一覧レイアウト調整

--- app/helpers/my_menus_helper.rb
+++ app/helpers/my_menus_helper.rb
@@ -1,2 +1,16 @@
 module MyMenusHelper
+  def genre_border_class(menu)
+    case menu.genre
+    when "staple" # 主食
+      "border-[#edbfbf]"
+    when "main"   # 主菜
+      "border-[#FFD4B1]"
+    when "side"   # 副菜
+      "border-[#b0d4bf]"
+    when "soup"   # 汁物
+      "border-[#F2E5B6]"
+    else         # その他
+      "border-[#C9DFEC]"
+    end
+  end
 end

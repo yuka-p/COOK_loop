43612853149f767b77eb9bc063b8ce7e68321515
yuka <214949744+yuka-p@users.noreply.github.com>
2026-01-14 16:05:42 +0900

    style: 献立表示ジャンル色分け

--- app/helpers/my_menus_helper.rb
+++ app/helpers/my_menus_helper.rb
@@ -13,4 +13,19 @@ module MyMenusHelper
       "border-[#C9DFEC]"
     end
   end
+
+def genre_heading_class(genre)
+    case genre
+    when "staple"
+      "bg-[#fad9e2]/50 text-gray-700 border-[#fad9e2]"
+    when "main"
+      "bg-[#edd4bf]/50 text-gray-700 border-[#edd4bf]"
+    when "side"
+      "bg-[#c9ded2]/50 text-gray-700 border-[#c9ded2]"
+    when "soup"
+      "bg-[#faf2c7]/50 text-gray-700 border-[#faf2c7]"
+    else
+      "bg-[#C9DFEC]/50 text-gray-700 border-[#C9DFEC]"
+    end
+  end
 end
